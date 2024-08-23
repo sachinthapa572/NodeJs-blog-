4fde310cbb4486c17ee5c81ff0015efbed2dfbe9
Sachin1Thapa <yousachin328gmail.com>
2024-08-23 22:47:51 +0545

    create authRoute.js

--- routes/BlogRoute.js
+++ routes/BlogRoute.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-
 const { homePage, deletePost, addPost, singleBlog, createPost, editPost, updatePost, showMyBlog } = require('../controller/blog/blogController');
-// const { signupPage, signupUser, loginPage, loginUser, logoutUser } = require('../controller/auth/authController');
 const { isAuth } = require('../middleware/isAuth.middleware');
 // multer  config 
 const { multer, storage } = require('../middleware/multer.middleware')
@@ -16,16 +14,5 @@ router.route('/deletepost/:id').get(isAuth, deletePost);
 router.route('/editpost/:id').get(isAuth, editPost);
 router.route('/updatepost/:id').post(isAuth, upload.single('image'), updatePost);
 router.route('/myBlog').get(isAuth, showMyBlog);
-
-//! user routes
-
-// router.route('/signuppage').get(signupPage);
-// router.route('/signupuser').post(signupUser);
-// router.route('/loginpage').get(loginPage);
-// router.route('/loginuser').post(loginUser);
-// router.route('/logout').get(logoutUser);
-
-
-
 
 module.exports = router;
